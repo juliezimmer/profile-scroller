08582cf4c004457244db2ce49ca643ca195ae542
Julie Zimmer <julie.berthiaume@gmail.comgit config --global user.email julie.berthiaume@gmail.com>
2018-03-10 23:48:25 -0600

    added automatic first profile loading when opening the scroller and when the end of the profile list is reached

--- app.js
+++ app.js
@@ -28,6 +28,9 @@ const data = [
 
 const profiles = profileIterator(data);
 
+// Call first profile - loads the DOM automatically with the first profile
+nextProfile();
+
 // click event for the Next button
 // when "Next" is clicked, we want to iterate through the profiles
 // Next Event
@@ -36,16 +39,22 @@ document.getElementById('next').addEventListener('click', nextProfile);
 // Next Profile Display
 function nextProfile() {
    const currentProfile = profiles.next().value;
-   document.getElementById('profileDisplay').innerHTML = `
-      <ul class="list-group">
-         <li class="list-group-item">Name: ${currentProfile.name}</li>
-         <li class="list-group-item">Age: ${currentProfile.age}</li>
-         <li class="list-group-item">Location: ${currentProfile.location}</li>
-         <li class="list-group-item">Preference: ${currentProfile.gender} looking for ${currentProfile.lookingfor}</li>
-      </ul>
+
+   if(currentProfile !== undefined) {
+      document.getElementById('profileDisplay').innerHTML = `
+         <ul class="list-group">
+            <li class="list-group-item">Name: ${currentProfile.name}</li>
+            <li class="list-group-item">Age: ${currentProfile.age}</li>
+            <li class="list-group-item">Location: ${currentProfile.location}</li>
+            <li class="list-group-item">Preference: ${currentProfile.gender} looking for ${currentProfile.lookingfor}</li>
+         </ul>
    `;
 
    document.getElementById('imageDisplay').innerHTML = `<img src="${currentProfile.image}">`;
+   } else {
+      // No more profiles
+      window.location.reload();
+   }
 }
 
 // Profile Iterator
