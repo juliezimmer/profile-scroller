91b06ea56e1532ac951040409ebe891c6d7e5d53
Julie Zimmer <julie.berthiaume@gmail.comgit config --global user.email julie.berthiaume@gmail.com>
2018-03-10 23:42:28 -0600

    profile displays pic and basic information for one three users in the small data set

--- app.js
+++ app.js
@@ -3,23 +3,26 @@ const data = [
    {
       name: "John Smith",
       age: 32,
+      gender: 'male',
       lookingfor: "female",
-      location: "BOston MA",
-      image: 'https://randomuser.me.api/portraits/men/82.jpeg'
+      location: "Boston MA",
+      image: 'https://randomuser.me/api/portraits/men/82.jpg'
    },
    {
       name: "Jen Andrews",
       age: 26,
+      gender: 'female',
       lookingfor: "male",
       location: "Miami FL",
-      image: 'https://randomuser.me.api/portraits/women/82.jpeg'
+      image: 'https://randomuser.me/api/portraits/women/82.jpg'
    },
    {
       name: "Ryan Johnson",
       age: 38,
+      gender: 'male',
       lookingfor: "female",
       location: "St. Paul MN",
-      image: 'https://randomuser.me.api/portraits/men/83.jpeg'
+      image: 'https://randomuser.me/api/portraits/men/83.jpg'
    },
 ];
 
@@ -32,7 +35,17 @@ document.getElementById('next').addEventListener('click', nextProfile);
 
 // Next Profile Display
 function nextProfile() {
-   
+   const currentProfile = profiles.next().value;
+   document.getElementById('profileDisplay').innerHTML = `
+      <ul class="list-group">
+         <li class="list-group-item">Name: ${currentProfile.name}</li>
+         <li class="list-group-item">Age: ${currentProfile.age}</li>
+         <li class="list-group-item">Location: ${currentProfile.location}</li>
+         <li class="list-group-item">Preference: ${currentProfile.gender} looking for ${currentProfile.lookingfor}</li>
+      </ul>
+   `;
+
+   document.getElementById('imageDisplay').innerHTML = `<img src="${currentProfile.image}">`;
 }
 
 // Profile Iterator
